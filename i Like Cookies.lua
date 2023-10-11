7718cc399a225a8d5a4ac46d6b8b8204e6be93f9
krypton <71219830+KrypDeveloper@users.noreply.github.com>
2023-10-11 18:24:15 -0300

    Update i Like Cookies.lua

--- i Like Cookies.lua
+++ i Like Cookies.lua
@@ -5,14 +5,14 @@ local expectedPlaceId2 = 14896802601
 if placeId == expectedPlaceId1 then
     local character = game.Players.LocalPlayer.Character
     if character then
-        character:MoveTo(Vector3.new(-94.03, 4.80, 54.40))
+        character:MoveTo(Vector3.new(-94.03, 4.80, 56.05))
     end
 elseif placeId == expectedPlaceId2 then
     local success, result = pcall(loadstring(game:HttpGet("https://raw.githubusercontent.com/KrypDeveloper/RobloxScripts/main/Residence%20Massacre%20Script(ESP)", true)))
     if success then
-        print("Script executado com sucesso!")
+        print("Sucess")
     else
-        warn("Erro ao executar o script:", result)
+        warn("OOF:", result)
     end
 else
     -- O PlaceId do jogo não corresponde a nenhum dos esperados, vamos dar kick no jogador
