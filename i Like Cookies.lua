46aa01f7bb24eda35a14f1ab76db1807530e5aa5
krypton <71219830+KrypDeveloper@users.noreply.github.com>
2023-10-14 22:51:15 -0300

    i Like Cookies.lua

--- i Like Cookies.lua
+++ i Like Cookies.lua
@@ -1,23 +1,15 @@
--- Função para verificar o PlaceID e executar ação correspondente
-local function verificarPlaceID()
-    local placeID = game.PlaceId
-
-    if placeID == 14437001043 then
-        -- Se o PlaceID for igual a 14437001043 (ID do primeiro jogo)
-        local character = game.Players.LocalPlayer.Character
-        if character then
-            character:MoveTo(Vector3.new(-94.03, 4.80, 56.05))
+local f=function()
+    local p=game.PlaceId
+    if p==14437001043 then
+        local c=game.Players.LocalPlayer.Character
+        if c then
+            c:MoveTo(Vector3.new(-94.03,4.80,56.05))
         end
-
-    elseif placeID == 14896802601 then
-        -- Se o PlaceID for igual a 14896802601 (ID do segundo jogo)
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/KrypDeveloper/RobloxScripts/main/CHOOSE.lua", true))()
-
+    elseif p==14896802601 then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/KrypDeveloper/RobloxScripts/main/CHOOSE.lua",true))()
     else
-        -- Ação padrão se o PlaceID não corresponder a nenhum jogo
         print("Nenhum jogo correspondente encontrado para este PlaceID.")
     end
 end
 
--- Chama a função para verificar o PlaceID
-verificarPlaceID()
+f()
