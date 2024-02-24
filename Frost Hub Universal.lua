2e6bb7d48b3ab340a934c40a35be426b55c6e46f
krypton <71219830+KrypDeveloper@users.noreply.github.com>
2024-02-24 18:37:31 -0300

    Create Frost Hub Universal.lua

---
+++ Frost Hub Universal.lua
@@ -0,0 +1,15 @@
+
+
+if game.PlaceId == 537413528 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KrypDeveloper/RobloxScripts/main/frost%20hub%20build%20a%20boat(BETA)"))()
+elseif game.PlaceId == 16365412402 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KrypDeveloper/RobloxScripts/main/PT-BR%20FROST%20HUB.lua"))()
+elseif game.PlaceId == 16091658541 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KrypDeveloper/RobloxScripts/main/PT-BR%20FROST%20HUB.lua"))()
+elseif game.PlaceId == 16129091908 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KrypDeveloper/RobloxScripts/main/PT-BR%20FROST%20HUB.lua"))()
+elseif game.PlaceId == 5210095481 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KrypDeveloper/RobloxScripts/main/PT-BR%20FROST%20HUB.lua"))()
+else
+print("você não esta em um jogo suportado\nYou are in a unsuported game")
+end
