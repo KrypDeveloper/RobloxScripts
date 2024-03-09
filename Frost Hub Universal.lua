91de2cb6febc661e4e555b28571ae0c831d70396
krypton <71219830+KrypDeveloper@users.noreply.github.com>
2024-03-09 16:49:56 -0300

    Update Frost Hub Universal.lua

--- Frost Hub Universal.lua
+++ Frost Hub Universal.lua
@@ -1,17 +1,25 @@
 
 
 if game.PlaceId == 537413528 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/KrypDeveloper/RobloxScripts/main/frost%20hub%20build%20a%20boat(BETA)"))()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/KrypDeveloper/RobloxScripts/main/frost%20hub%20build%20a%20boat(BETA)"))()
 elseif game.PlaceId == 16365412402 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/KrypDeveloper/RobloxScripts/main/PT-BR%20FROST%20HUB.lua"))()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/KrypDeveloper/RobloxScripts/main/PT-BR%20FROST%20HUB.lua"))()
 elseif game.PlaceId == 16091658541 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/KrypDeveloper/RobloxScripts/main/PT-BR%20FROST%20HUB.lua"))()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/KrypDeveloper/RobloxScripts/main/PT-BR%20FROST%20HUB.lua"))()
 elseif game.PlaceId == 16129091908 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/KrypDeveloper/RobloxScripts/main/PT-BR%20FROST%20HUB.lua"))()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/KrypDeveloper/RobloxScripts/main/PT-BR%20FROST%20HUB.lua"))()
 elseif game.PlaceId == 5210095481 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/KrypDeveloper/RobloxScripts/main/PT-BR%20FROST%20HUB.lua"))()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/KrypDeveloper/RobloxScripts/main/PT-BR%20FROST%20HUB.lua"))()
 elseif game.PlaceId == 189707 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/KrypDeveloper/RobloxScripts/main/Frost%20Hub%20%7C%20Natural%20Disaster.lua"))()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/KrypDeveloper/RobloxScripts/main/Frost%20Hub%20%7C%20Natural%20Disaster.lua"))()
+elseif game.Placeid == 14437001043 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/KrypDeveloper/RobloxScripts/main/Frost%20Hub%20Residence%20Massacre"))()
+elseif game.PlaceId == 16667550979 then
+ loadstring(game:HttpGet("https://raw.githubusercontent.com/KrypDeveloper/RobloxScripts/main/Frost%20Hub%20Residence%20Massacre"))()
+elseif game.PlaceId == 14896802601 then
+ loadstring(game:HttpGet("https://raw.githubusercontent.com/KrypDeveloper/RobloxScripts/main/Frost%20Hub%20Residence%20Massacre"))()
+elseif game.PlaceId == 14896802601 then
+ loadstring(game:HttpGet("https://raw.githubusercontent.com/KrypDeveloper/RobloxScripts/main/Frost%20Hub%20Residence%20Massacre"))()
 else
 print("você não esta em um jogo suportado\nYou are in a unsuported game")
 end
